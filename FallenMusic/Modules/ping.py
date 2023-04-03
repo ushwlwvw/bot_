4698bd314444ff59814b80f85b6807f6fc6fe2d4
ushwlwvw <126856143+ushwlwvw@users.noreply.github.com>
2023-04-03 03:11:27 +0300

    Update ping.py

--- FallenMusic/Modules/ping.py
+++ FallenMusic/Modules/ping.py
@@ -32,10 +32,10 @@ from FallenMusic import BOT_NAME, StartTime, app
 from FallenMusic.Helpers import get_readable_time
 
 
-@app.on_message(filters.command("ping") | filters.command(["بنج","البنج"],prefixes= ["/", "!","","#"]))
+@app.on_message(filters.command("ping") | filters.command(["بنك","البنك"],prefixes= ["/", "!","","#"]))
 async def ping_fallen(_, message: Message):
     hmm = await message.reply_photo(
-        photo=config.PING_IMG, caption=f"{BOT_NAME} ⚡"
+        photo=config.PING_IMG, caption=f"{BOT_NAME} انتظر..."
     )
     upt = int(time.time() - StartTime)
     cpu = psutil.cpu_percent(interval=0.5)
@@ -46,21 +46,21 @@ async def ping_fallen(_, message: Message):
     uptime = get_readable_time((upt))
 
     await hmm.edit_text(
-        f"""⎊ آلبنج : `{resp}ᴍs`
+        f"""⌔︙ آلبنج : `{resp}ᴍs`
 
 <b><u>{BOT_NAME} آلحآله :</u></b>
 
-⎊ **مدة التشغيل :** {uptime}
-⎊ **الرام :** {mem}
-⎊ **وحدة المعالجة المركزية :** {cpu}
-⎊ **القرص :** {disk}""",
+⌔︙ **مدة التشغيل :** {uptime}
+⌔︙ **الرام :** {mem}
+⌔︙ **وحدة المعالجة المركزية :** {cpu}
+⌔︙ **القرص :** {disk}""",
         reply_markup=InlineKeyboardMarkup(
             [
                 [
                     InlineKeyboardButton("الدعم", url=config.SUPPORT_CHAT),
                     InlineKeyboardButton(
                         "السوࢪس",
-                        url="https://t.me/EE_20",
+                        url="https://t.me/aaaalqp",
                     ),
                 ],
             ]
